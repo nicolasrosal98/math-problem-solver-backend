ccb950fc3a609552f2c821ee550c65ef656fb34a
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-01-27 10:27:21 +0000

    Create POST / endpoint to allow user to send files

--- server.ts
+++ server.ts
@@ -1,35 +1,63 @@
-import { Client } from "pg";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
+import fileUpload = require("express-fileupload")
 
-config(); //Read .env file lines as though they were env vars.
+// ML
+import * as tf from '@tensorflow/tfjs';
 
-//Call this script with the environment variable LOCAL set if you want to connect to a local db (i.e. without SSL)
-//Do not set the environment variable LOCAL if you want to connect to a heroku DB.
+// async function modelpredict(image: string) {
+//     const MODEL_URL = 'https://raw.githubusercontent.com/vipul79321/Handwritten-Equation-Solver/master/model_final.json';
+//     const model = await tf.loadGraphModel(MODEL_URL)
+//     return model.predict(image)
+// }
 
-//For the ssl property of the DB connection config, use a value of...
-// false - when connecting to a local DB
-// { rejectUnauthorized: false } - when connecting to a heroku DB
-const herokuSSLSetting = { rejectUnauthorized: false }
-const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
-const dbConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: sslSetting,
-};
+config(); 
 
 const app = express();
 
-app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
-
-const client = new Client(dbConfig);
-client.connect();
+app.use(express.json());
+app.use(cors());
+app.use(fileUpload());
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+  res.send({
+    status: true,
+    message: 'This works'
+  });
 });
+
+app.post('/upload-mathproblem', (req, res) => {
+  if (req.files) {
+    console.log(req.files)
+  }
+})
+
+// app.post('/upload-mathproblem', async (req, res) => {
+//   try {
+//       if(!req.files) {
+//           res.send({
+//               status: false,
+//               message: 'No file uploaded'
+//           });
+//       } else {
+//           let mathproblem = req.files.mathproblem as fileUpload.UploadedFile;
+        
+//           res.send({
+//               status: true,
+//               message: 'File is uploaded',
+//               data: {
+//                   name: mathproblem.name,
+//                   mimetype: mathproblem.mimetype,
+//                   size: mathproblem.size
+//               },
+//               // formula: modelpredict(mathproblem)
+//           });
+//       }
+//   } catch (err) {
+//       res.status(500).send(err);
+//   }
+// });
 
 
 //Start the server on the given port
@@ -40,3 +68,4 @@ if (!port) {
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
+
