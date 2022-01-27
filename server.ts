1bd51ece3bcfb56978501a98bde21df4309da53d
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-01-27 13:17:44 +0000

    Add CI (Prettier + Lint + Build)

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@ import { Client } from "pg";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
-import fileUpload = require("express-fileupload")
+import fileUpload = require("express-fileupload");
 
-config(); 
+config();
 
 const herokuSSLSetting = { rejectUnauthorized: false };
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
@@ -25,15 +25,15 @@ client.connect();
 app.get("/", async (req, res) => {
   res.send({
     status: true,
-    message: 'This works'
+    message: "This works",
   });
 });
 
-app.post('/upload-mathproblem', async (req, res) => {
+app.post("/upload-mathproblem", async (req, res) => {
   try {
     if (req.files) {
-      let mathUpload = req.files[''] as fileUpload.UploadedFile
-      let problemName = mathUpload.name
+      const mathUpload = req.files[""] as fileUpload.UploadedFile;
+      const problemName = mathUpload.name;
       const query = await client.query(
         "insert into photo_problem (filename) values ($1) returning *;",
         [problemName]
@@ -42,20 +42,19 @@ app.post('/upload-mathproblem', async (req, res) => {
     } else {
       res.status(400).send({
         status: false,
-        message: 'No file uploaded'
-      })
+        message: "No file uploaded",
+      });
     }
   } catch (error) {
     res.status(500).send(error);
   }
-})
+});
 
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
-  throw 'Missing PORT environment variable.  Set it in .env file.';
+  throw "Missing PORT environment variable.  Set it in .env file.";
 }
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
-
